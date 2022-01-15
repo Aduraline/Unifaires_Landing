9f355aa648f3bf0acee6f3454861d5d60d17d9ab
Enoch Olutunmida <eolutunmida@gmail.com>
2022-01-15 09:49:08 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   // distDir: '../../dist/functions/next'
   images: {
     domains: ['pbs.twimg.com'],
-    loader: "static",
+    loader: 'custom',
+    path: 'https://pbs.twimg.com',
   },
 };
 
